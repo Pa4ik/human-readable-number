ec2fb6fdd8c4e0be655d0acf46a5b5040692bbac
Maxim Pachuzheuski <makspohp123@gmail.com>
2024-09-11 02:02:29 +0300

    feat:finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+    const unitsAndTeens = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const arr = number.toString().split('');
+
+    if (number < 20) {
+        return unitsAndTeens[number];
+    } else if (number < 100) {
+        return number % 10 === 0 ? tens[arr[0] - 2] : tens[arr[0] - 2] + " " + unitsAndTeens[arr[1]];
+    } else if (number >= 100 && number < 1000) {
+        if (number === 100) {
+            return 'one hundred'
+        };
+        return number % 100 === 0 ? unitsAndTeens[arr[0]] + ' hundred' : unitsAndTeens[arr[0]] + ' hundred ' + toReadable(+arr.slice(1).join(''));
+    }
 }
